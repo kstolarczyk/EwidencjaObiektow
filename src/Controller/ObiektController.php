b2abf3b6c8c40fda1ec7318260b5999f75f681e4
pawel <bora911@interia.pl>
2020-04-24 11:40:51 +0200

    ObiektController

---
+++ src/Controller/ObiektController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\GrupaObiektow;
+use App\Entity\Obiekt;
+use App\Form\ObiektType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class ObiektController extends AbstractController
+{
+    /**
+     * @Route("/obiekt", name="obiekt_index")
+     */
+    public function index(Request $request, EntityManagerInterface $entityManager)
+    {
+        $lista = $entityManager->getRepository(Obiekt::class)->findAll();
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse($this->renderView('obiekt/tabela.html.twig', ['lista' => $lista]));
+        }
+        return $this->render('obiekt/index.html.twig', [
+            'lista' => $lista
+        ]);
+    }
+
+    /**
+     * @Route("/obiekt/Dodaj", name="obiekt_dodaj", condition="request.isXmlHttpRequest()", methods={"POST"})
+     */
+    public function dodaj(Request $request, EntityManagerInterface $entityManager)
+    {
+        $obiekt = new Obiekt();
+        //$obiekt->setParametry(null);
+        $form = $this->createForm(ObiektType::class, $obiekt);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($obiekt);
+            $entityManager->flush();
+            return new JsonResponse(true);
+        }
+
+        return new JsonResponse($this->renderView('obiekt/form.html.twig', ['form' => $form->createView()]));
+    }
+
+
+    /**
+     * @Route("/obiekt/Edytuj/{id}", name="obiekt_edytuj", condition="request.isXmlHttpRequest()", requirements={"id":"\d+"}, methods={"POST"})
+     */
+    public function edytuj(Request $request, EntityManagerInterface $entityManager, Obiekt $obiekt)
+    {
+        $form = $this->createForm(Obiekt::class, $obiekt);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return new JsonResponse(true);
+        }
+
+        return new JsonResponse($this->renderView('obiekt/form.html.twig', ['form' => $form->createView()]));
+    }
+
+}
